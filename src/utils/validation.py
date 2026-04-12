62dfde3492da759449b3e2c14a1613433841f1e7
Joshan john <joshanjohn2003@gmail.com>
2026-04-12 13:59:45 +0100

    (chore) set initial name from email address and refactor token validation

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -3,20 +3,25 @@ from fastapi.responses import RedirectResponse
 from google.auth.transport import requests
 from google.oauth2.id_token import verify_firebase_token
 
+from src.utils.variables import logger
 
 firebase_request_adapter = requests.Request()
 
 def token_validation(token: str):
     if not token:
+        logger.error("No Token Found!")
         response = RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
+        logger.info("Clear token cookies.. ")
         response.delete_cookie("token")
         return response
 
     try:
         data = verify_firebase_token(token, firebase_request_adapter)
+        logger.info("Token validated successfully")
         return data
 
-    except Exception:
+    except Exception as e:
+        logger.error(f"Error while validating token: {str(e)}")
         response = RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
         response.delete_cookie("token")
         return response
